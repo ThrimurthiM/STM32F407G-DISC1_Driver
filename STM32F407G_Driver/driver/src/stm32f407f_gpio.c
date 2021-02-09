088c1700ed2939f99ad43bbd360c34601f32cd0d
ThrimurthiM <43003017+ThrimurthiM@users.noreply.github.com>
2021-02-09 13:48:53 +0530

    Update stm32f407f_gpio.c

--- STM32F407G_Driver/driver/src/stm32f407f_gpio.c
+++ STM32F407G_Driver/driver/src/stm32f407f_gpio.c
@@ -140,7 +140,7 @@ void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
 }
 
 
-uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber)
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, GPIO_PinNum_t pinNumber)
 {
 	return (uint8_t)((pGPIOx->IDR >> pinNumber) & 0x01);
 }
